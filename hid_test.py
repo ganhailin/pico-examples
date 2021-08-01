9fed0f295f299d323e186ed8d535629079ec6431
tinylib <3dgans@outlook.com>
2021-08-02 04:19:56 +0800

    fix hidapi

---
+++ hid_test.py
@@ -0,0 +1,24 @@
+# Install python3 HID package https://pypi.org/project/hid/
+import hid
+
+USB_VID = 0xcafe
+
+print("Openning HID device with VID = 0x%X" % USB_VID)
+
+for dict in hid.enumerate(USB_VID):
+    print(dict)
+    dev = hid.device()
+    dev.open(dict['vendor_id'], dict['product_id'])
+    if dev:
+        
+        while True:
+            
+            # Get input from console and encode to UTF8 for array of chars.
+            str_out = input("Send text to HID Device : ").encode('utf-8')
+            print(str_out)
+            sent=dev.write(str_out)
+            str_in = dev.read(64)
+            print(sent)
+            print(str_in)
+            print("Received from HID Device:", str_in, '\n')
+
